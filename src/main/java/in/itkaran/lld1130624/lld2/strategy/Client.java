3778e394c88ddee849ef91f96485fc0bf3f51b38
Kumar Karan <contact.karan.ai@gmail.com>
2024-07-16 17:41:49 +0530

    Change method name in strategy example

--- src/main/java/in/itkaran/lld1130624/lld2/strategy/Client.java
+++ src/main/java/in/itkaran/lld1130624/lld2/strategy/Client.java
@@ -6,15 +6,15 @@ public class Client {
     public static void main(String[] args) {
         demoStrategy();
     }
-    private static PathCalculator findStrategy(ModeType mode) {
+    private static PathCalculator getStrategy(ModeType mode) {
         if (mode.equals(ModeType.CAR)) return new CarPathCalculator();
         else if (mode.equals(ModeType.BIKE)) return new BikePathCalculator();
         else if (mode.equals(ModeType.WALK)) return new WalkPathCalculator();
         return null;
     }
     private static void demoStrategy() {
-        ModeType mode = ModeType.BIKE;
-        PathCalculator pathCalculator = findStrategy(mode);
+        ModeType mode = ModeType.CAR;
+        PathCalculator pathCalculator = getStrategy(mode);
         GoogleMap googleMap = new GoogleMap(pathCalculator);
         googleMap.findPath("Kashmir", "Kanyakumari");
     }
